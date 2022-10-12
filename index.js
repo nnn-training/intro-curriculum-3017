30863f9a0380952a8a4b37df01c2c4e744d565a7
maiamea <maiameawork@gmail.com>
2022-10-12 16:32:09 +0900

    ログアウト用のリンクを作成

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const server = http.createServer(basic.check((req, res) => {
       'Content-Type': 'text/plain; charset=utf-8'
     });
     res.end('ログアウトしました');
+    console.info('ログアウトしました');
     return;
   }
 
